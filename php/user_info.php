128dffd7d8c2d04a7a5e7e38d84e1759ed4cc5eb
Panagiotis Leos <97064473+PanagiotisLeos@users.noreply.github.com>
2023-09-15 12:51:50 +0300

    added user points history

--- php/user_info.php
+++ php/user_info.php
@@ -45,6 +45,47 @@ if($x==2){
 
 }   
 
+if($x==3){
+    $query1 = "SELECT  product.name , user.username , react , timestamp 
+    from ratings
+    INNER JOIN offers on offers.id = ratings.offer_id
+    INNER JOIN product on offers.product_id = product.id
+    INNER JOIN user on user.user_id = offers.user_id
+    where user.user_id = $id";
+
+    $result=mysqli_query($conn,$query1);
+    
+    $userRatings=[];
+    while($r=mysqli_fetch_assoc($result))
+    {
+        $userRatings[]=$r;
+    }
+    echo json_encode($userRatings);
+
+}  
+
+
+if($x==4){
+    $query = "SELECT score , tokens , 
+    (SELECT sum(added_score)  FROM score_history WHERE score_history.user_id = user.user_id AND MONTH(timestamp) = MONTH(now())) as cm_score  , 
+    (SELECT sum(added_tokens)  FROM token_history WHERE token_history.user_id = user.user_id AND MONTH(timestamp) = MONTH(now())-1) as lm_tokens
+    FROM user 
+    WHERE user.user_id = $id";
+
+    $result=mysqli_query($conn,$query);
+    $data = mysqli_fetch_assoc($result);
+
+    $userPoints = [];
+    $userPoints['score'] = $data['score'];
+    $userPoints['cm_score'] = $data['cm_score'];
+    $userPoints['tokens'] = $data['tokens'];
+    $userPoints['lm_tokens'] = $data['lm_tokens'];
+
+    $userPointsJson = json_encode($userPoints);
+
+    echo $userPointsJson;
+
+}  
 
     
 
